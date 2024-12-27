90c6bb7710c7ed2216313e64818c073364002f6b
Usman Khan <usman.k1219@gmail.com>
2024-12-27 03:35:03 -0500

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import DarkModeToggle from "./components/DarkModeToggle"; 
 import NavBar from "./components/NavBar";
